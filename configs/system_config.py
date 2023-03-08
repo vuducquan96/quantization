a68872314be01d56b0707e05ab7cdcf070421a9c
vuducquan96 <vuducquan96@gmail.com>
2023-03-09 07:54:36 +0900

    [add] dataset config

---
+++ configs/system_config.py
@@ -0,0 +1,14 @@
+number_worker_per_gpu = 3
+sleeping_time = 0
+reset_cluster_database = False
+opt_level = 'O0'
+
+# Optimization level
+# O0 fp 32
+# O1 mix fp 16
+# O2 fp16
+
+debugging = False
+showing_frequency = 10
+time_check = False
+pretrain = 'resume'
