fe3bdaf14c90b422ac6b1b6f7309a5cbb4f60227
vuducquan96 <vuducquan96@gmail.com>
2023-03-11 00:16:59 +0900

    [add] builders model

--- core/models/builders.py
+++ core/models/builders.py
@@ -1,17 +1,11 @@
 from pdb import set_trace as bp
 from core.models.registry import LOSS, MODELS
 from core.models.utils import VoxelGenerator
-# from spconv.utils import PointToVoxel as VoxelGenerator
-from core.loss.loss_lib import (WeightedSmoothL1LocalizationLoss,
-                                SigmoidFocalClassificationLoss)
-
-
 def model_builder(model_config, data_config, box_coders=None, anchor_box_function=None):
 
     voxel_generator = VoxelGenerator(
         voxel_size=data_config["VOXEL_GENERATOR"]["VOXEL_SIZE"],
         point_cloud_range=data_config["POINT_CLOUD_RANGE"],
-        # num_point_features=3, 
         max_num_points_per_voxel=data_config["VOXEL_GENERATOR"]["MAX_NUMBER_OF_POINTS_PER_VOXEL"],
         max_num_voxels=data_config["VOXEL_GENERATOR"]["MAX_VOXELS"]
     )
