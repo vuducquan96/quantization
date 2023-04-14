9de61acfcd499b1b68acdba5448f3f5effda175e
vuducquan96 <vuducquan96@gmail.com>
2023-04-14 15:52:55 +0900

    [add]

---
+++ core/models/detectors/__init__.py
@@ -0,0 +1,7 @@
+from .bev import base2d
+from .bev_mobvit import basemobvit
+from .resnet_unet import  resnetunet
+from .resnet_unet_temlate import resnetunettemplate
+from .seresnet_fpn import seresnetfpn
+from .pillarnet import pillarset
+from .pointbase import cpupointbase
