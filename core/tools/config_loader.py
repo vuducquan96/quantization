a2f6a9c9bd90ad20aee31fb30712258de026ddad
vuducquan96 <vuducquan96@gmail.com>
2023-04-14 15:52:28 +0900

    [add] config loader

--- core/tools/config_loader.py
+++ core/tools/config_loader.py
@@ -3,11 +3,9 @@ import toml
 import yaml
 from easydict import EasyDict
 from pdb import set_trace as bp
-from core.torchie.utils import Config
 
 
 def config_loader(file_path):
-
     if ".json" in file_path.lower():
         with open(file_path) as json_file:
             data = json.load(json_file)
@@ -23,8 +21,4 @@ def config_loader(file_path):
         with open(file_path, 'r') as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
         data = EasyDict(data)
-        return data
-
-    if ".py" in file_path.lower():
-        data = Config.fromfile(file_path)
         return data
